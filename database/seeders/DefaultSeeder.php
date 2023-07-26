30caf3cfacfc766e13eb18fbfe490484b5511dbd
mhmmd95 <mohammadalabras77@gmail.com>
2023-07-26 19:12:26 +0300

    added: roles seeder

---
+++ database/seeders/DefaultSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+declare (strict_types = 1);
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+use App\Enums\Role;
+
+final class DefaultSeeder extends Seeder
+{
+    public function run(): void
+    {
+        //admin
+        DB::table('roles')->insert([
+            'id' => Role::ADMIN->value,
+            'title' => 'admin',
+            'description' => 'a full-control over the system role'
+        ]);
+
+        //editor
+        DB::table('roles')->insert([
+            'id' => Role::EDITOR->value,
+            'title' => 'editor',
+            'description' => 'a role to edit and update existed users accounts'
+        ]);
+
+        //user
+        DB::table('roles')->insert([
+            'id' => Role::USER->value,
+            'title' => 'user',
+            'description' => 'the default role, responsible only on its account'
+        ]);
+    }
+}
