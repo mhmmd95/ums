ca7544e15c6b782e51373288e97b5ea9ea7b4e7f
mhmmd95 <mohammadalabras77@gmail.com>
2023-07-28 20:44:37 +0300

    implemented: user search and filter via email and name

--- app/Http/Controllers/Users/IndexController.php
+++ app/Http/Controllers/Users/IndexController.php
@@ -4,19 +4,26 @@ declare (strict_types = 1);
 
 namespace App\Http\Controllers\Users;
 
+use Illuminate\Database\Eloquent\Builder;
 use \Illuminate\Contracts\View\Factory;
 use \Illuminate\Contracts\View\View;
 use App\Http\Controllers\Controller;
-use App\Models\Role;
+use Illuminate\Http\Request;
 use App\Models\User;
 
 final class IndexController extends Controller
 {
-    public function __invoke(): View|Factory {
+    public function __invoke(Request $request): View|Factory {
 
         $this->authorize('viewAny', User::class);
 
-        $users = User::clients()->paginate(8);
+        $users = User::clients()
+            ->when($request->has('name'), function(Builder $query) use ($request){
+                $query->where('name', 'like', '%'.$request->name.'%');
+            })->when($request->has('email'), function(Builder $query) use ($request){
+                $query->where('email', 'like', '%'.$request->email.'%');
+        })->paginate($request->perpage ?? 8)->withQueryString();
+
         return view('users.index', compact('users'));
     }
 }
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -5,11 +5,33 @@
         </h2>
     </x-slot>
 
+    <div class="py-1 mt-4">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <div>
+                        <form method='get' action="{{route('dashboard.users.index')}}" class='md:flex md:justify-between'>
+                            <div>
+                                <label for='perpage'>Number of rows: </label>
+                                <x-text-input class="mt-1" type="number" name="perpage" :value="request('perpage', 8)" autocomplete="perpage" />
+                            </div>
+                            <div>
+                                <x-text-input class="mt-1" type="text" name="name" :value="request('name')" placeholder="Search for a name" autocomplete="name" />
+                                <x-text-input class="mt-1" type="text" name="email" :value="request('email')" placeholder="Search for an email" autocomplete="email" />
+                            </div>
+                            <x-primary-button>{{ __('filter') }}</x-primary-button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <x-users.index_table :users="$users"/>
+                    <x-users.index_table :users="$users" />
                 </div>
             </div>
         </div>
